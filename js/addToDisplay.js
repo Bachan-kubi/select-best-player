1a80919049ef040582959778525d86e7d0d8c814
bachan kubi <felixghagra@gmail.com>
2022-08-22 19:02:31 +0600

    setting condition

--- js/addToDisplay.js
+++ js/addToDisplay.js
@@ -4,24 +4,17 @@ function showSeletedPlayer(player){
     
     const tableBody = document.getElementById('player-list');
     tableBody.innerHTML = '';
-    for(i=0; i<player.length; i++){
-        // warning message selecting more than 5 players
-        if(player.length>4){
-            alert('Selection Limit exceeded!');
-            return;
-        }else{
-            const playerName = player[i];
-        
+    
+    for(i=0; i<player.length; i++){        
+        const playerName = player[i];       
         // create tr and dispaly on table body
         const tr = document.createElement('tr');
         tr.innerHTML = `
         <td>${i+1}</td>
         <td>${playerName}</td>`;
-        tableBody.appendChild(tr);
-        }
+        tableBody.appendChild(tr); 
     }
 }
-
 function addToCart(element){
     const playerName = element.parentNode.children[0].innerText;
     playerArr.push(playerName);
