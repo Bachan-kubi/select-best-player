9eefe41f22398653ac5e903a130582b4342963b1
bachan kubi <felixghagra@gmail.com>
2022-08-22 20:39:59 +0600

    commenting on fucntions

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,4 @@
+// retrive value from input
 function getElementValueById(inputId){
     const inputElement = document.getElementById(inputId);
     const inputElementStr = inputElement.value;
@@ -5,6 +6,7 @@ function getElementValueById(inputId){
     inputElement.value = '';
     return inputValue; 
 }
+// to get value from text targeted
 
 function getTextValue(textId){
     const textElement = document.getElementById(textId);
@@ -13,7 +15,7 @@ function getTextValue(textId){
     return textAmount;
 }
 
-// to set values
+// to set values to targeted text field
 function setValuesById(textId, value){
     const txtIdElement = document.getElementById(textId);
     txtIdElement.innerText = value; 
