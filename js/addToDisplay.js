5bb4009a77dbedee1c0878b2b2d50358ac3e4921
bachan kubi <felixghagra@gmail.com>
2022-08-23 18:21:13 +0600

    second final commit

--- js/addToDisplay.js
+++ js/addToDisplay.js
@@ -1,10 +1,20 @@
+//player in an array. 
 const playerArr = [];
 
 function showSeletedPlayer(player){
     
     const tableBody = document.getElementById('player-list');
     tableBody.innerHTML = '';
-    for(i=0; i<player.length; i++){        
+    
+    for(i=0; i<player.length; i++){     
+        
+        // show warning message
+        if(i>4){
+            alert('selection limit exceeded!');
+            i.pop();
+            return; 
+        }
+        
         const playerName = player[i];       
         // create tr and dispaly on table body
         const tr = document.createElement('tr');
@@ -14,15 +24,17 @@ function showSeletedPlayer(player){
         tableBody.appendChild(tr); 
         
         //warning message for selecting more than 5 players!
-        if(tableBody.childElementCount>4){
-            alert('this is the last selection! Dont select more than 5!');
-            return; 
-        }
-
+        // if(tableBody.childElementCount>4){
+        //     alert('this is the last selection! Dont select more than 5!');
+        //     return; 
+        // }
     }
 }
+
 function addToCart(element){
+    
     const playerName = element.parentNode.children[0].innerText;
+    
     playerArr.push(playerName);
      
     // showing player
